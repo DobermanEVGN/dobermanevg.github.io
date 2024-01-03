7952a52f94e1258395e747e38840505497d4168a
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-01-03 21:24:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ $(function() {
     minDate: '+2M',
     maxDate: '+4M'
   });
+
+  $("#departure, #arrival").autocomplete({
+    source: function(request, response) {
+      $.ajax({
+        url: "https://autocomplete.travelpayouts.com/places2",
+        dataType: "json",
+        data: {
+          locale: "ru",
+          types: ["airport", "city"],
+          term: request.term
+        },
+        success: function(data) {
+          var results = [];
+          for (var i = 0; i < data.length; i++) {
+            // Форматирование данных для отображения в выпадающем списке
+            var label = data[i].name + ' - ' + data[i].code;
+            results.push({ label: label, value: data[i].name });
+          }
+          response(results);
+        }
+      });
+    },
+    minLength: 3
+  });
 });
