05ac530eac83a37aeda7f54b8edab4330f50eba8
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-01-06 22:14:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
 $(function() {
 
-  $("#depart-date, #return-date").datepicker({
-    dateFormat: 'yy-mm-dd',
-    minDate: 0,
-    maxDate: '+2M', 
-    defaultDate: '+1w',
-    language: 'ru'
-  });
-
+  // Автокомплит для полей ввода городов
   $("#departure, #arrival").autocomplete({
+
+    // Источник данных 
     source: function(request, response) {
+    
       $.ajax({
+      
         url: "https://autocomplete.travelpayouts.com/places2",
-        dataType: "json",
+        
+        dataType: "json", 
+        
         data: {
           locale: "ru",
           types: ["airport", "city"],
           term: request.term
         },
+
         success: function(data) {
-          var results = [];
-          for (var i = 0; i < Math.min(data.length, 5); i++) {
-            var label = data[i].name + ' - ' + data[i].code;
-            results.push({
-              label: label,
-              value: data[i].name,
-              airportCode: data[i].code
-            });
-          }
-          response(results);
+
+        // Ограничиваем до 5 элементов
+        var results = [];
+        for(var i = 0; i < Math.min(data.length, 5); i++){
+
+          var item = data[i];
+          var label = item.name + ' - ' + item.code;
+          
+          results.push({
+             label: label,
+             value: item.name,
+             code: item.code
+          });
+
         }
+        
+        response(results);
+      }
       });
     },
-    minLength: 3,
+
+    // Обработчик выбора элемента
     select: function(event, ui) {
-      $(this).siblings(".airport-code").remove();
-      $("<span class='airport-code'>" + ui.item.airportCode + "</span>").insertAfter($(this)); 
-    }
+      
+      // Устанавливаем значение поля равным названию города
+      $(this).val(ui.item.value);
+
+      // Остальная логика  
+      var id = this.id === "departure" ? "departure-hint" : "arrival-hint";
+      $("#" + id).text("Вы выбрали: " + ui.item.label);
+      return false;
+    },
+
+    minLength: 3
+    
+  });
+  
+  
+    
+
+
+  
+
+  $("#passenger-btn").click(function() {
+    // код для модального окна с выбором пассажиров
   });
 
 });
