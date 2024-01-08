0bace4e1e542df5166da8dab94153b946aac7526
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-01-08 22:17:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,147 @@
 $(function() {
 
-  // Автокомплит для полей ввода городов
   $("#departure, #arrival").autocomplete({
-
-    // Источник данных 
     source: function(request, response) {
-    
       $.ajax({
-      
-        url: "https://autocomplete.travelpayouts.com/places2",
-        
-        dataType: "json", 
-        
+        url: "https://autocomplete.travelpayouts.com/places2", 
+        dataType: "json",
         data: {
           locale: "ru",
           types: ["airport", "city"],
-          term: request.term
+          term: request.term  
         },
-
         success: function(data) {
-
-        // Ограничиваем до 5 элементов
-        var results = [];
-        for(var i = 0; i < Math.min(data.length, 5); i++){
-
-          var item = data[i];
-          var label = item.name + ' - ' + item.code;
-          
-          results.push({
-             label: label,
-             value: item.name,
-             code: item.code
-          });
-
-        }
         
-        response(results);
-      }
+          var results = [];
+          
+          for(var i = 0; i < Math.min(data.length, 5); i++){
+
+            var item = data[i];
+            
+            var label = item.name + ' - ' + item.code;
+            
+            results.push({
+              label: label,
+              value: item.name,
+              code: item.code
+            });
+
+          }
+        
+          response(results);  
+        }
       });
     },
-
-    // Обработчик выбора элемента
     select: function(event, ui) {
+      $(this).val(ui.item.value);  
       
-      // Устанавливаем значение поля равным названию города
-      $(this).val(ui.item.value);
-
-      // Остальная логика  
       var id = this.id === "departure" ? "departure-hint" : "arrival-hint";
+      
       $("#" + id).text("Вы выбрали: " + ui.item.label);
+      
       return false;
     },
-
-    minLength: 3
-    
+    minLength: 3  
   });
-  
-  
-    
 
+  var adultsCount = 1;
+  var childrenCount = 0;
+  var infantsCount = 0;
 
+// Обновление информации о количестве пассажиров и классе
+function updateCounts() {
+  $("#adultsCount").text(adultsCount);
+  $("#childrenCount").text(childrenCount);
+  $("#infantsCount").text(infantsCount);
+  $("#isBusinessClass").text(isBusinessClass ? "Да" : "Нет");
   
+  // Проверка для блокировки кнопки "-" у "12 лет и старше" 
+  if (adultsCount === infantsCount) {
+    $("#adultsMinusBtn").prop('disabled', true);
+  } else {
+    $("#adultsMinusBtn").prop('disabled', false);
+  }
+}
+
+var isBusinessClass = false;
+
+$("#businessClassCheckbox").change(function() {
+  isBusinessClass = $(this).is(":checked");
+  updateCounts();
+});
+
+  $("#adultsMinusBtn").click(function() {
+    if (adultsCount > 1) {
+      adultsCount--;
+      updateCounts();
+    }
+  });
+
+  $("#adultsPlusBtn").click(function() {
+    if (adultsCount + childrenCount + infantsCount < 9) {
+      adultsCount++;
+      updateCounts();
+    }
+  });
+
+  $("#childrenMinusBtn").click(function() {
+    if (childrenCount > 0) {
+      childrenCount--;
+      updateCounts();
+    }
+  });
+
+  $("#childrenPlusBtn").click(function() {
+    if (adultsCount + childrenCount + infantsCount < 9 && adultsCount + childrenCount < 9) {
+      childrenCount++;
+      updateCounts();
+    }
+  });
+
+  $("#infantsMinusBtn").click(function() {
+    if (infantsCount > 0) {
+      infantsCount--;
+      updateCounts();
+    }
+  });
+
+  $("#infantsPlusBtn").click(function() {
+    if (infantsCount < adultsCount && adultsCount + childrenCount + infantsCount < 9) {
+      infantsCount++;
+      updateCounts();
+    }
+  });
+
+  // ... (existing code)
+
+  var minDate = new Date();
+  
+  var maxDate = moment().add(2, 'months').toDate();
+
+  $("#depart-date").datepicker({
+    dateFormat: 'yyyy-mm-dd',
+    minDate: minDate,
+    maxDate: maxDate,
+    autoClose: true
+  });
+
+  $("#return-date").datepicker({
+     dateFormat: 'yyyy-mm-dd',
+     minDate: minDate,  
+     maxDate: maxDate,
+     autoClose: true
+  });
+   
+  var adultsCount = 1;
+
+  $("#adultsCount").text(adultsCount);
 
   $("#passenger-btn").click(function() {
-    // код для модального окна с выбором пассажиров
+    $("#modal").show(); 
   });
+
+  $(".close").click(function() {
+    $("#modal").hide();
+  });  
 
 });
