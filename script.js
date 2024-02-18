c96fd502345dd8710a7bb97f4a61135d59a60c89
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-02-18 18:20:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -188,6 +188,7 @@ $(function() {
     let msg = `Откуда: ${departure.value}\nКуда: ${arrival.value}\nДата 1: ${departDate.value}\nДата 2: ${returnDate.value}\nEmail: ${email.value}\n\nПассажиры:\nВзрослых: ${adultsCount}\nДетей: ${childrenCount}\nМладенцев: ${infantsCount}\nКласс: ${isBusinessClass ? 'Бизнес' : 'Эконом'}`;
 
     const tg = window.Telegram.WebApp;
+    tg.expand(); //расширяем на все окно
 
     tg.MainButton.text = 'Отправить в Telegram';
     tg.MainButton.isVisible = true; 
