a90099d0fae5f61caccab605d37f24e77b52f7c7
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-02-19 17:16:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ tg.MainButton.text = 'Отправить в Telegram';
 tg.MainButton.isVisible = true; 
 tg.MainButton.show(); 
 
-tg.MainButton.onclick = function() {
-  tg.sendData('Test message'); 
+tg.MainButton.onclick = function() { 
   tg.close()
 }
 
 $(function() {
+  let tg = window.Telegram.WebApp;
+  tg.expand(); //расширяем на все окно
+
+  
+
+    
+
+  tg.MainButton.text = 'Отправить в Telegram';
+  tg.MainButton.isVisible = true; 
+  tg.MainButton.show(); 
+
+  tg.MainButton.onclick = function() { 
+  tg.close()
+  }
 
   $("#departure, #arrival").autocomplete({
     source: function(request, response) {
