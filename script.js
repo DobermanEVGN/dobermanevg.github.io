32347b4d10f4cbe69392f4e3dd7d2f78df16b087
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-02-19 16:05:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
   tg.MainButton.show(); 
 
   tg.MainButton.onclick = function() {
-  tg.sendData(msg);
+  tg.sendData('Test message');
   }
 });
 $(function() {
