edb465a0dafd5c92733de3587597bd81bfe270d6
DobermanEVGN <155563026+DobermanEVGN@users.noreply.github.com>
2024-02-21 15:23:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.MainButton.hide()
+});
+
 $(function() {
 
   $("#departure, #arrival").autocomplete({
